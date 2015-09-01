e8888f3f87321ec34424301346904c8a16604b1b
Ferry Wang <foomango@gmail.com>
2015-09-01 19:57:00 +0800

    Implementing Database Persistence

---
+++ first_spring/db/load_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (id, description, price) VALUES(1, 'Lamp', 5.78);
+INSERT INTO products (id, description, price) VALUES(2, 'Table', 75.29);
+INSERT INTO products (id, description, price) VALUES(3, 'Chair', 22.81);
---
+++ first_spring/db/server.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+java -classpath ../war/WEB-INF/lib/hsqldb.jar org.hsqldb.Server -database test
